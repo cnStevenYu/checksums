64d9df055f832c7ba5fb7a7202257c64d73d57fc
cnStevenYu <397729364@qq.com>
2015-03-16 16:27:39 +0800

    replace with statement with try statement more pythonic

--- checksums.py
+++ checksums.py
@@ -4,12 +4,14 @@ import argparse
 
 def argsParse(arguments):
 	"""checksums.py files -a md5
-	usage: checksums.py file [file ...] [-a md5]
+	usage: checksums.py FILE [FILE ...] [-a ALGORITHM]
 	positional arguments:
+		FILE: file to hash 
 
-	optional arguments: a
-		
+	optional arguments: 
+		ALGORITHM: md5, sha256, sha1 supported
 	"""
+
 	parser = argparse.ArgumentParser(description='checksums')
 	parser.add_argument('-a', '--algorithm', action='store',  
 						default='md5', dest='algorithm', help='hash algorithm to be used' )
@@ -19,13 +21,12 @@ def argsParse(arguments):
 
 def main():	
 	args = argsParse(sys.argv[1:])
-
-	files = args.files
-	algorithm = args.algorithm
+	files, algorithm = args.files, args.algorithm
 
 	for fileTohash in files:
 		checksum = Checksums(fileTohash, algorithm)
-		print(fileTohash + ":" + checksum.cal())
+		#print(fileTohash + ":" + checksum.cal())
+		print(''.join([fileTohash, ':', checksum.cal()]))
 
 class Checksums:
 	"""calculate md5, sha1, sha256..."""
@@ -46,19 +47,13 @@ class Checksums:
 
 	def cal(self):
 		tool = self.getHashTool() 
-		try:
-			f = open(self.fileTohash, 'rb')
-			try:
 
-				while True:
-					data = f.read(128)
-					if not data:
-						break
-					tool.update(data)
-			finally:
-				f.close()
-		except IOError:
-			print "io error "
+		with open(self.fileTohash) as f:
+			while True:
+				data = f.read(128)
+				if not data:
+					break
+				tool.update(data)
 
 		return tool.hexdigest()
 
